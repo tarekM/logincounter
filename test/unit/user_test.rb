2483b75e744907bf08ed98ae59cc1318f0daef14
TarekM <tal-mouhtasseb@berkeley.edu>
2014-02-14 13:04:10 -0800

    Finished project.

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -11,17 +11,64 @@ class UserTest < ActiveSupport::TestCase
 	ERR_BAD_PASSWORD = -4
 	MAX_USERNAME_LENGTH = 128
 	MAX_PASSWORD_LENGTH = 128
+	LONG_STRING = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 
-	rake db:test:prepare
+	test "try signup" do
+		out = User.signup("user", "ilove162")
+		assert out == SUCCESS
+	end
+
+	test "try login" do
+		out1 = User.signup("user", "ilove162")
+		out = User.login("user","ilove162")
+		assert out == SUCCESS
+	end
+
+	test "bad username" do
+		out = User.signup("","hello")
+		assert out == ERR_BAD_USERNAME
+	end
+
+	test "bad password" do
+		out = User.signup("newguy","")
+		assert out == ERR_BAD_PASSWORD
+	end
+
+	test "signupexists" do
+		out1 = User.signup("user","password")
+		out = User.signup("user","password")
+		assert out == ERR_USER_EXISTS
+	end
+
+	test "wrong password" do
+		out = User.login("user","ihate162")
+		assert out == ERR_BAD_CREDENTIALS
+	end
 
 	test "reset" do
 		out = User.TESTAPI_resetFixture 
 		assert out == SUCCESS
 	end
 
-	test "signup succeeds" do
-		val = User.add("test1", "test")
-		assert val == SUCCESS
+	test "add multiple users" do
+		out = User.signup("alan","turing")
+		assert out == SUCCESS
+
+		out = User.signup("brian","harvey")
+		assert out == SUCCESS
 	end
+
+	test "long password" do
+		out = User.signup("g",LONG_STRING)
+		assert out == ERR_BAD_PASSWORD
+	end
+
+	test "long username" do
+		out = User.signup(LONG_STRING,"pw")
+		assert out == ERR_BAD_USERNAME
+	end
+
+
+
 
 end
